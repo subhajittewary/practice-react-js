de2c339dda0b2318b1649f864dd118100e3db10d
Subhajit Tewary <subhajittewary800@outlook.com>
2025-03-18 17:11:28 +0530

    chore: Update React imports and optimize code formatting

---
+++ ecommerce-base/src/shopping-cart-context.jsx
@@ -0,0 +1,96 @@
+import { createContext } from "react";
+import { DUMMY_PRODUCTS } from "./dummy-products";
+import { useReducer } from "react";
+
+export const CartContext = createContext({
+  items: [],
+  addItemToCart: () => {},
+  updateItemQuantity: () => {},
+});
+
+function shoppingCartReducer(state, action) {
+  switch (action.type) {
+    case "ADD_ITEM": {
+      const updatedItems = [...state.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === action.payload
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find(
+          (product) => product.id === action.payload
+        );
+        updatedItems.push({
+          id: action.payload,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
+      return {
+        items: updatedItems,
+      };
+    }
+    case "UPDATE_ITEM": {
+      const updatedItems = [...state.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === action.payload.productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += action.payload.quantity;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    }
+  }
+}
+
+export default function CartContextProvider({ children }) {
+  const [shoppingCart, dispatchSoppingCart] = useReducer(shoppingCartReducer, {
+    items: [],
+  });
+
+  function handleAddItemToCart(id) {
+    dispatchSoppingCart({ type: "ADD_ITEM", payload: id });
+  }
+
+  function handleUpdateCartItemQuantity(productId, quantity) {
+    dispatchSoppingCart({
+      type: "UPDATE_ITEM",
+      payload: {
+        productId,
+        quantity,
+      },
+    });
+  }
+
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart,
+    updateItemQuantity: handleUpdateCartItemQuantity,
+  };
+
+  return (
+    <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
+  );
+}
